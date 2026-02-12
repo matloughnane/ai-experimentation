6d48b053a677606bd9e44965505984833364b6d7
Matthew <mat.loughnane@gmail.com>
2026-02-12 10:26:29 +0000

    Rotated posthog API

--- mastra_server/src/posthog/index.ts
+++ mastra_server/src/posthog/index.ts
@@ -1,6 +1,5 @@
 import { PostHog } from "posthog-node";
 
-export const phClient = new PostHog(
-  "phc_lmj0uIZ1Sp9ilO97dfvbla6jt3QXggl9JMzNICaA0UZ",
-  { host: "https://eu.i.posthog.com" },
-);
+export const phClient = new PostHog(process.env.POSTHOG_API_KEY ?? "", {
+  host: "https://eu.i.posthog.com",
+});
